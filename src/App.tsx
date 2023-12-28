7b08b318415d818bae050e4eefc624699b3e6dd7
Francisco Perez Lefiman <fperez.sdev@gmail.com>
2023-12-28 14:30:27 -0300

    Agregar barra de navegacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { useState } from 'react';
+import Home from './pages/Home';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
-      <h1 className="text-red-500">Daily Regards</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-      </div>
+      <Home />
     </div>
   );
 }
